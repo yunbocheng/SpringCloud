4ff8e98789a7c2afc7efb18d9458f738f762c9d7
yunbocheng <2484720940@qq.com>
2024-02-26 23:32:42 +0800

    feat: :tada:新增支付模块

---
+++ cloud-provider-payment8001/src/main/java/com/yunbocheng/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.yunbocheng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : YunboCheng
+ * @date : 23:28 2024/2/26
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
